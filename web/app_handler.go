67c2759568dd7489cd8244768c631f8e0961053b
SkylakeCoder <skylakecoder@gmail.com>
2016-11-21 00:52:06 +0800

    refactoring in app_handler.

--- web/app_handler.go
+++ web/app_handler.go
@@ -23,7 +23,7 @@ func newAppHandler(settings *appSettings) *appHandler {
 }
 
 func (handler *appHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
-	url := req.URL.String()
+	url := req.URL.Path
 	reqHandler, exist := handler.patternMap[url]
 	if exist {
 		reqHandler.HandleRequest(
@@ -36,47 +36,51 @@ func (handler *appHandler) ServeHTTP(res http.ResponseWriter, req *http.Request)
 			},
 		)
 	} else {
-		served := false
 		// check the situation /xx/:xxx
-		for k, v := range handler.patternMap {
-			matches := handler.routerRuleReg.FindAllString(k, -1)
-			if len(matches) != 1 {
-				continue
-			}
-			urlSplits := strings.Split(url, "/")
-			urlSplitsLen := len(urlSplits)
-			if urlSplitsLen < 2 {
-				continue
-			}
-			fixedURL := ""
-			for i := 0; i < urlSplitsLen-1; i++ {
-				fixedURL += urlSplits[i]
-				if i < urlSplitsLen-2 {
-					fixedURL += "/"
-				}
-			}
-			k = strings.Replace(k, matches[0], "", -1)
-			if k == fixedURL {
-				paramKey := strings.Replace(matches[0], "/:", "", -1)
-				paramValue := urlSplits[len(urlSplits)-1]
-				params, _ := NewKeyValues(paramKey, paramValue)
-				v.HandleRequest(
-					&Request{
-						req, params,
-					},
-					&Response{
-						res,
-						handler.settings,
-					},
-				)
-				served = true
-				break
+		if handler.checkPatternColon(res, req) {
+			return
+		}
+		log.Printf("unrecognized url: %s\n", url)
+	}
+}
+
+func (handler *appHandler) checkPatternColon(res http.ResponseWriter, req *http.Request) bool {
+	url := req.URL.Path
+	for k, v := range handler.patternMap {
+		matches := handler.routerRuleReg.FindAllString(k, -1)
+		if len(matches) != 1 {
+			continue
+		}
+		urlSplits := strings.Split(url, "/")
+		urlSplitsLen := len(urlSplits)
+		if urlSplitsLen < 2 {
+			continue
+		}
+		fixedURL := ""
+		for i := 0; i < urlSplitsLen-1; i++ {
+			fixedURL += urlSplits[i]
+			if i < urlSplitsLen-2 {
+				fixedURL += "/"
 			}
 		}
-		if !served {
-			log.Printf("unrecognized url: %s\n", url)
+		k = strings.Replace(k, matches[0], "", -1)
+		if k == fixedURL {
+			paramKey := strings.Replace(matches[0], "/:", "", -1)
+			paramValue := urlSplits[len(urlSplits)-1]
+			params, _ := NewKeyValues(paramKey, paramValue)
+			v.HandleRequest(
+				&Request{
+					req, params,
+				},
+				&Response{
+					res,
+					handler.settings,
+				},
+			)
+			return true
 		}
 	}
+	return false
 }
 
 func (handler *appHandler) addPatternHandler(pattern string, reqHandler RequestHandler) error {
